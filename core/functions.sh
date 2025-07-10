73702b02c441ae3ed6fc518d85002711614ac8e9
steve-conrad <steve.conrad14@gmail.com>
2025-07-10 10:46:23 -0400

    added wallpaper setup to config_setup function

--- core/functions.sh
+++ core/functions.sh
@@ -130,8 +130,13 @@ detect_sensors() {
 }
 
 config_setup() {
+#Copy config files to the user's home directory
   cp -r "$HOME/.config/hyprnosis/themes/Default/." "$HOME/.config/"
   cp -r "$HOME/.config/hyprnosis/configs/hypr/." "$HOME/.config/hypr/"
   cp -r "$HOME/.config/hyprnosis/configs/nvim/." "$HOME/.config/nvim/"
   cp -r "$HOME/.config/hyprnosis/configs/waybar/." "$HOME/.config/waybar/"
+#Clone wallpaper repo
+  git clone --depth 1 https://github.com/steve-conrad/hyprnosis-wallpapers.git /tmp/wallpapers && \
+  cp -r /tmp/wallpapers/. "$HOME/.config/hyprnosis/wallpapers/" && \
+  rm -rf /tmp/wallpapers
 }
