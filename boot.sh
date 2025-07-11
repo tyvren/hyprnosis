c4a06e45df49d66854b11cd63d32cbed02f05062
steve-conrad <steve.conrad14@gmail.com>
2025-07-11 11:09:09 -0400

    added a check for repo at start of script to allow re-runs

--- boot.sh
+++ boot.sh
@@ -19,8 +19,12 @@ print_logo
 pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
 INSTALL_DIR="$HOME/.config/hyprnosis"
-git clone --depth 1 https://github.com/steve-conrad/hyprnosis.git "$INSTALL_DIR"
 
+if [ -d "$INSTALL_DIR/.git" ]; then
+  echo "Repository already exists at $INSTALL_DIR. Skipping clone."
+else
+  git clone --depth 1 https://github.com/steve-conrad/hyprnosis.git "$INSTALL_DIR"
+fi
 
 echo "Starting Hyprnosis installation..."
 cd "$INSTALL_DIR"
