8dbe12bed0907ef3d3d1475372e0e0fd36bab803
steve-conrad <steve.conrad14@gmail.com>
2025-07-11 09:20:12 -0400

    updated default nvim colors

--- themes/Default/nvim/colorscheme.lua
+++ themes/Default/nvim/colorscheme.lua
@@ -1,21 +1,23 @@
 vim.cmd("highlight clear")
 vim.o.background = "dark"
-vim.g.colors_name = "blacklight"
+vim.g.colors_name = "hyprnosis"
 
 local colors = {
-  bg       = "#0b0014",  -- deep blackened purple
-  fg       = "#d6c9ff",  -- soft glowing lavender for readability
-  accent1  = "#c084fc",  -- vibrant neon purple
-  accent2  = "#a855f7",  -- deep electric violet
-  gray     = "#4b3f59",  -- muted gray-purple
+  bg       = "#000002",  -- near-black
+  fg       = "#fafafa",  -- soft white
+  accent1  = "#fe93d7",  -- pink
+  accent2  = "#9f96d9",  -- lavender
+  accent3  = "#90A9E1",  -- light blue
+  accent4  = "#BFFCFD",  -- cyan
+  gray     = "#8f97a2",  -- gray-blue
 }
 
 vim.api.nvim_set_hl(0, "Normal",        { fg = colors.fg, bg = colors.bg })
 vim.api.nvim_set_hl(0, "Comment",       { fg = colors.gray, italic = true })
 vim.api.nvim_set_hl(0, "Constant",      { fg = colors.accent2 })
-vim.api.nvim_set_hl(0, "String",        { fg = colors.accent1 })
+vim.api.nvim_set_hl(0, "String",        { fg = colors.accent4 })
 vim.api.nvim_set_hl(0, "Function",      { fg = colors.accent2 })
-vim.api.nvim_set_hl(0, "Identifier",    { fg = colors.accent1 })
+vim.api.nvim_set_hl(0, "Identifier",    { fg = colors.accent3 })
 vim.api.nvim_set_hl(0, "Statement",     { fg = colors.accent1, bold = true })
 vim.api.nvim_set_hl(0, "PreProc",       { fg = colors.accent2 })
 vim.api.nvim_set_hl(0, "Type",          { fg = colors.fg })
@@ -30,4 +32,3 @@ vim.api.nvim_set_hl(0, "StatusLine",    { fg = colors.fg, bg = colors.gray })
 vim.api.nvim_set_hl(0, "Pmenu",         { fg = colors.fg, bg = colors.gray })
 
 return {}
-
