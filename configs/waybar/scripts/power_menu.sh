7ad68682ec740da2e54e26611e2932ed130f7b44
steve-conrad <steve.conrad14@gmail.com>
2025-07-13 12:01:17 -0400

    updated power menu

--- configs/waybar/scripts/power_menu.sh
+++ configs/waybar/scripts/power_menu.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-choice=$(printf "  Shutdown\n  Restart\n  Logout\n  Lock" | wofi --dmenu --width 300 --height 410 --cache-file /dev/null)
+choice=$(printf "  Shutdown\n  Restart\n  Lock" | wofi --dmenu --width 300 --height 250 --cache-file /dev/null)
 
 case "$choice" in
     "  Shutdown") systemctl poweroff ;;
     "  Restart") systemctl reboot ;;
-    "  Logout") hyprctl dispatch exit ;;
     "  Lock") hyprlock ;;
 esac
