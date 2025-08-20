7c5c282c7daf8cc50b42296c68c6a20ef483f6bd
steve-conrad <steve.conrad14@gmail.com>
2025-08-20 15:23:37 -0400

    added hyprnosis menu to start the unified menu system buildout

--- modules/hyprnosis_menu.sh
+++ modules/hyprnosis_menu.sh
@@ -1,11 +1,23 @@
-items="  Power Menu\n  Update\nKeybinds"
-output=$(echo -e $items | walker --dmenu -H)
-if [[ "$output" == "  Power Menu" ]]; then
-	exec ~/hyprnosis/modules/power_walker.sh
-elif [[ "$output" == "  Update" ]]; then
-	exec ~/hyprnosis/modules/hyprnosis_update.sh	
-elif [[ "$output" == "Keybinds" ]]; then
-	exec ~/hyprnosis/modules/wofi_keybinds.sh	
-else
-	echo "Please select a command"
-fi
+#!/bin/bash
+
+menu() {
+    local prompt="$1"
+    local options="$2"
+    echo -e "$options" | walker --dmenu -H -p "$prompt…"
+}
+
+present_terminal() {
+    ghostty -e bash -c "$1"
+}
+
+main_items="  Power Menu\n  Update\n Keybinds"
+
+selection=$(menu "Hyprnosis Menu" "$main_items")
+
+case "$selection" in
+    *Power*) exec ~/hyprnosis/modules/power_walker.sh ;;
+    *Update*) exec ~/hyprnosis/modules/hyprnosis_update.sh ;;
+    *Keybinds*) exec ~/hyprnosis/modules/wofi_keybinds.sh ;;
+    *) echo "Please select a command" ;;
+esac
+
