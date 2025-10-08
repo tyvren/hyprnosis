906d6477d44db03f4e2752253f70247259595c40
steve-conrad <steve.conrad14@gmail.com>
2025-10-07 20:58:32 -0400

    updated monitor setup script to account for multiple displays

--- modules/hyprnosis_tui.sh
+++ modules/hyprnosis_tui.sh
@@ -178,26 +178,47 @@ case "$main_choice" in
 		"Config TUI")
 	    	    clear
 	    	    config_path="$HOME/.config/hypr/settings/monitors.conf"
-
 	    	    monitors=$(hyprctl monitors)
 
-	    	    type=$(echo "$monitors" | grep "Monitor" | awk '{print $2}')
+		    readarray -t connection_types < <(echo "$monitors" | grep "^Monitor" | awk '{print $2}')
+		    readarray -t model_names < <(echo "$monitors" | grep "model" | awk '{print $3}')
 
-	    	    modes=$(echo "$monitors" | grep "availableModes" | sed 's/^[[:space:]]*availableModes: //')
+		    display_list=()
+		    for i in "${!connection_types[@]}"; do
+    		        display_list+=("${connection_types[$i]} - ${model_names[$i]}")
+		    done
 
-	    	    mode_list=$(echo "$modes" | tr ' ' '\n')
+		    gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
+		    "Select the monitor to configure"
+		    monitor_choice=$(printf "%s\n" "${display_list[@]}" | gum choose --limit=1)
 
-	    	    gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
-  	    	    "Select the resolution and refresh rate"
-	    	    chosen_mode=$(echo "$mode_list" | gum choose --limit=1)
-	     	    clear
+		    selected_connection=$(echo "$monitor_choice" | awk '{print $1}')
 
-	    	    gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
-  	    	    "Select the scale"
-	    	    chosen_scale=$(printf "1\n1.5\n2\n" | gum choose --limit=1)
+		    monitor_info=$(echo "$monitors" | awk "/Monitor $selected_connection/,/^$/" )
 
-	    	    sed -i "s/^monitor=.*/monitor=${type},${chosen_mode},auto,${chosen_scale}/" $config_path
-	    	    gum confirm "Press enter to return to menu." && exec "$0"
+		    modes=$(echo "$monitor_info" | grep "availableModes" | sed 's/^[[:space:]]*availableModes: //')
+		    mode_list=$(echo "$modes" | tr ' ' '\n')
+
+		    gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
+		    "Select the resolution and refresh rate"
+		    chosen_mode=$(echo "$mode_list" | gum choose --limit=1)
+		    clear
+
+		    gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
+		    "Select the monitor's position"
+		    position=$(printf "auto\nauto-left\nauto-right\n" | gum choose --limit=1)
+		    clear
+
+		    gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
+		    "Select the scale"
+		    chosen_scale=$(printf "1\n1.5\n2\n" | gum choose --limit=1)
+		    clear
+
+		    sed -i "/^monitor=${selected_connection}/d" "$config_path"
+
+		    sed -i "/^monitor=,preferred,auto, 1/a monitor=${selected_connection},${chosen_mode},${position},${chosen_scale}" "$config_path"
+
+		    gum confirm "Press enter to return to menu." && exec "$0"
 		    ;;
 
 		"Manual Config")
--- modules/monitor_setup.sh
+++ modules/monitor_setup.sh
@@ -1,34 +1,45 @@
 #A simple gum tui for managing a single hyprland monitor config
-
 clear
 
 config_path="$HOME/.config/hypr/settings/monitors.conf"
-
-#Capture full hyprctl monitor info
 monitors=$(hyprctl monitors)
 
-#Capture monitor connection type
-type=$(echo "$monitors" | grep "Monitor" | awk '{print $2}')
+readarray -t connection_types < <(echo "$monitors" | grep "^Monitor" | awk '{print $2}')
+readarray -t model_names < <(echo "$monitors" | grep "model" | awk '{print $3}')
 
-#Capture monitor modes
-modes=$(echo "$monitors" | grep "availableModes" | sed 's/^[[:space:]]*availableModes: //')
+display_list=()
+for i in "${!connection_types[@]}"; do
+    display_list+=("${connection_types[$i]} - ${model_names[$i]}")
+done
 
-#Turn modes into a list
+gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
+"Select the monitor to configure"
+monitor_choice=$(printf "%s\n" "${display_list[@]}" | gum choose --limit=1)
+
+selected_connection=$(echo "$monitor_choice" | awk '{print $1}')
+
+monitor_info=$(echo "$monitors" | awk "/Monitor $selected_connection/,/^$/" )
+
+modes=$(echo "$monitor_info" | grep "availableModes" | sed 's/^[[:space:]]*availableModes: //')
 mode_list=$(echo "$modes" | tr ' ' '\n')
 
-#Choose resolution and refresh rate
 gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
-  "Select the resolution and refresh rate"
+"Select the resolution and refresh rate"
 chosen_mode=$(echo "$mode_list" | gum choose --limit=1)
-
 clear
 
-#Choose scaling
 gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
-  "Select the scale"
-chosen_scale=$(printf "1\n1.5\n2\n" | gum choose --limit=1)
+"Select the monitor's position"
+position=$(printf "auto\nauto-left\nauto-right\n" | gum choose --limit=1)
+clear
 
-#Write to hyprland monitor config
-sed -i "s/^monitor=.*/monitor=${type},${chosen_mode},auto,${chosen_scale}/" $config_path
+gum style --foreground 99 --border double --align center --margin "1 2" --padding "2 4" \
+"Select the scale"
+chosen_scale=$(printf "1\n1.5\n2\n" | gum choose --limit=1)
+clear
+
+sed -i "/^monitor=${selected_connection}/d" "$config_path"
+
+sed -i "/^monitor=,preferred,auto, 1/a monitor=${selected_connection},${chosen_mode},${position},${chosen_scale}" "$config_path"
 
 gum confirm "Press enter to return to menu." && exec "$0"
