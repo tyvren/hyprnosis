bacd6e2546724417f9ee75fca63016af451b1455
steve-conrad <steve.conrad14@gmail.com>
2025-10-13 18:05:02 -0400

    randmoized wallpaper now applies to hyprpaper and hyprlock configs to persist after reboots

--- modules/randomize_wallpaper.sh
+++ modules/randomize_wallpaper.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/.config/hyprnosis/wallpapers/Mocha/"
+HYPRPAPER_CONF="$HOME/.config/hypr/hyprpaper.conf"
+HYPRLOCK_CONF="$HOME/.config/hypr/hyprlock.conf"
+
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
@@ -8,3 +11,10 @@ WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" 
 
 # Apply the selected wallpaper
 hyprctl hyprpaper reload ,"$WALLPAPER"
+
+# Update Hyprpaper config
+sed -i "s|^preload = .*|preload = $WALLPAPER|" "$HYPRPAPER_CONF"
+sed -i "s|^wallpaper = .*|wallpaper = ,$WALLPAPER|" "$HYPRPAPER_CONF"
+
+# Update Hyprlock config
+sed -i "/background {/,/}/{s|^\s*path = .*|  path = $WALLPAPER|}" "$HYPRLOCK_CONF"
