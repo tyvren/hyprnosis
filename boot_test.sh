0f5855e7fed363be1a48d3d3c48381c810146109
steve-conrad <steve.conrad14@gmail.com>
2025-10-22 18:16:22 -0400

    testing

--- boot_test.sh
+++ boot_test.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 set -e
-
-# Ensure Gum is installed first
-if ! command -v gum &>/dev/null; then
-    echo "Installing gum..."
-    sudo pacman -Sy --noconfirm gum
-fi
-
+print_logo() {
+	cat << "EOF"
+ ██░ ██▓██   ██▓ ██▓███   ██▀███   ███▄    █  ▒█████    ██████  ██▓  ██████ 
+▓██░ ██▒▒██  ██▒▓██░  ██▒▓██ ▒ ██▒ ██ ▀█   █ ▒██▒  ██▒▒██    ▒ ▓██▒▒██    ▒ 
+▒██▀▀██░ ▒██ ██░▓██░ ██▓▒▓██ ░▄█ ▒▓██  ▀█ ██▒▒██░  ██▒░ ▓██▄   ▒██▒░ ▓██▄   
+░▓█ ░██  ░ ▐██▓░▒██▄█▓▒ ▒▒██▀▀█▄  ▓██▒  ▐▌██▒▒██   ██░  ▒   ██▒░██░  ▒   ██▒
+░▓█▒░██▓ ░ ██▒▓░▒██▒ ░  ░░██▓ ▒██▒▒██░   ▓██░░ ████▓▒░▒██████▒▒░██░▒██████▒▒
+ ▒ ░░▒░▒  ██▒▒▒ ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░▓  ▒ ▒▓▒ ▒ ░
+ ▒ ░▒░ ░▓██ ░▒░ ░▒ ░       ░▒ ░ ▒░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░▒  ░ ░ ▒ ░░ ░▒  ░ ░
+ ░  ░░ ░▒ ▒ ░░  ░░         ░░   ░    ░   ░ ░ ░ ░ ░ ▒  ░  ░  ░   ▒ ░░  ░  ░  
+ ░  ░  ░░ ░                 ░              ░     ░ ░        ░   ░        ░  
+EOF
+}
 clear
-gum style --bold --underline "Hyprnosis Installer"
+print_logo
 
-spinner "Installing git..." sudo pacman -Sy --noconfirm --needed git
+sudo pacman -Sy --noconfirm --needed git
 
 INSTALL_DIR="$HOME/.config/hyprnosis"
-log_step "Cloning Hyprnosis repository"
-spinner "Cloning hyprnosis repo..." git clone https://github.com/tyvren/hyprnosis.git "$INSTALL_DIR"
+echo -e "\nCloning hyprnosis"
+git clone https://github.com/tyvren/hyprnosis.git "$INSTALL_DIR"
 
-log_step "Starting Hyprnosis installation"
-cd "$INSTALL_DIR" || exit
+echo "Starting Hyprnosis installation..."
+cd "$INSTALL_DIR"
 chmod +x ./hyprnosis_test.sh
-spinner "Running primary installer..." bash ./hyprnosis_test.sh
+source ./hyprnosis_test.sh
