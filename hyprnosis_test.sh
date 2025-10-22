5eb9dff26e6e99a949d2e05992bbb4263c386d85
steve-conrad <steve.conrad14@gmail.com>
2025-10-22 19:37:33 -0400

    testing

--- hyprnosis_test.sh
+++ hyprnosis_test.sh
@@ -10,47 +10,60 @@ log_header "hyprnosis"
 
 create_log
 
+clear
 log_step "Getting username"
 get_username
 
+clear
 log_step "Updating system packages"
 spinner "Updating system..." sudo pacman -Syu --noconfirm
 
+clear
 log_step "Installing yay AUR helper"
 install_yay
 
+clear
 log_step "Installing GPU packages"
 install_gpu_packages
 
+clear
 log_step "Installing system utilities"
 install_packages "${system_utils[@]}"
 
+clear
 log_step "Installing application packages"
 install_packages "${app_packages[@]}"
 
+clear
 log_step "Installing Hyprland packages"
 install_packages "${hypr_packages[@]}"
 
+clear
 log_step "Enabling essential services"
 enable_service "networkmanager"
 enable_service "bluetooth.service"
 enable_service "cups"
 enable_service "lm_sensors"
 
+clear
 log_step "Enabling user services"
 enable_user_service "waybar.service"
 enable_elephant_service
 enable_walker_service
 
+clear
 log_step "Setting up configuration"
 config_setup
 
+clear
 log_step "Enabling Plymouth theme"
 enable_plymouth
 
+clear
 log_step "Setting up hyprnosis alias"
 setup_hyprnosis_alias
 
+clear
 log_step "Enabling Hyprland autologin"
 hyprland_autologin
 
