edceba605b968cf7403dd43dfc927b5bf790aebe
tyvren <steve.conrad14@gmail.com>
2025-10-30 23:04:30 -0400

    added new gum update script and linked it to the menu. expanded the selection when searching the dmenu launcher.

---
+++ modules/update_system.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clear
+
+header() {
+  gum style \
+    --foreground 37 --border-foreground 69 --border double \
+    --align center --width 50 --margin "1 0" --padding "0 2" \
+    'System Updates'
+}
+
+prompt() {
+  local text="$1"
+  gum style --foreground 69 "$1"
+}
+
+repo_updates=$(pacman -Qu 2>/dev/null)
+aur_updates=$(yay -Qua 2>/dev/null)
+
+header
+gum spin --spinner dot --title "Checking for updates..." -- sleep 2
+
+if [[ -z "$repo_updates" && -z "$aur_updates" ]]; then
+  prompt "No available updates."
+  exit 0
+else
+  sudo -v
+  gum spin --spinner dot --title "Updates found, now installing..." -- yay -Syu --noconfirm --show-output
+
+  prompt "Updates Installed."
+  prompt "$repo_updates $aur_updates"
+  running_kernel=$(uname -r | sed 's/-arch/\.arch/')
+  installed_kernel=$(pacman -Q linux | awk '{print $2}')
+
+  if [[ "$running_kernel" != "$installed_kernel" ]]; then
+    header
+    prompt "Kernel update detected."
+    if gum confirm "Reboot now to apply the new kernel?"; then
+      sudo reboot
+    fi
+  fi
+fi
