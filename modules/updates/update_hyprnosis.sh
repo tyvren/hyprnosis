766de7276732ee5f8f959668bd077d6def1d24be
tyvren <steve.conrad14@gmail.com>
2025-11-19 22:46:07 -0500

    removed extra quote causing update script to error

--- modules/updates/update_hyprnosis.sh
+++ modules/updates/update_hyprnosis.sh
@@ -20,7 +20,7 @@ prompt() {
 
 header
 
-gum spin --spinner dot --title "Fetching updates for hyprnosis..." -- git -C "$INSTALL_DIR" fetch origin 
+gum spin --spinner dot --title "Fetching updates for hyprnosis..." -- git -C "$INSTALL_DIR" fetch origin
 gum spin --spinner dot --title "Resetting repo to main branch" -- git -C "$INSTALL_DIR" reset --hard origin/main
 
 gum spin --spinner dot --title "Updating wallpapers..." -- git clone --depth 1 https://github.com/tyvren/hyprnosis-wallpapers.git /tmp/wallpapers
@@ -31,6 +31,6 @@ rm -rf /tmp/wallpapers
 prompt "Updating elephant/walker configs..."
 rm -r $HOME/.config/walker
 cp -ru $CONFIG_DIR/walker "$LOCAL_CONFIG"
-cp -ru $CONFIG_DIR/elephant "$LOCAL_CONFIG""
+cp -ru $CONFIG_DIR/elephant "$LOCAL_CONFIG"
 
 gum confirm "Update complete. Press enter to close."
