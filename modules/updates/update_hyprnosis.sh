a021e4f212a07e4556a68cc91a66a831d5f7591a
tyvren <steve.conrad14@gmail.com>
2025-11-28 11:39:36 -0500

    changed hyprnosis update script to grab existing theme name before updating walker and elephant files, then re-applies the theme.

--- modules/updates/update_hyprnosis.sh
+++ modules/updates/update_hyprnosis.sh
@@ -3,6 +3,7 @@
 INSTALL_DIR="$HOME/.config/hyprnosis"
 CONFIG_DIR="$HOME/.config/hyprnosis/config"
 LOCAL_CONFIG="$HOME/.config"
+THEME_DIR="$INSTALL_DIR/themes"
 
 clear
 
@@ -13,20 +14,36 @@ header() {
     'hyprnosis update'
 }
 
+spin() {
+  gum spin --spinner dot --title "$1"
+}
+
+prompt() {
+  gum style --foreground 69 "$1"
+}
+
 header
 
-gum spin --spinner dot --title "Fetching updates for hyprnosis..." -- git -C "$INSTALL_DIR" fetch origin
-gum spin --spinner dot --title "Resetting repo to main branch" -- git -C "$INSTALL_DIR" reset --hard origin/main
+spin "Fetching updates for hyprnosis" -- git -C "$INSTALL_DIR" fetch origin
+spin "Resetting repo to main branch" -- git -C "$INSTALL_DIR" reset --hard origin/main
 
-gum spin --spinner dot --title "Updating wallpapers..." -- git clone --depth 1 https://github.com/tyvren/hyprnosis-wallpapers.git /tmp/wallpapers
+spin "Updating wallpapers" -- git clone --depth 1 https://github.com/tyvren/hyprnosis-wallpapers.git /tmp/wallpapers
 rm -rf /tmp/wallpapers/.git
 rm -rf /tmp/wallpapers/README.md
 cp -r /tmp/wallpapers/. "$INSTALL_DIR/wallpapers/"
 rm -rf /tmp/wallpapers
 
-gum style --foreground 69 "Updating elephant/walker configs..."
+current_theme=$(grep '^theme = ' ~/.config/walker/config.toml | sed -E 's/theme = "([^"]+)".*/\1/')
+
+prompt "Updating elephant and walker configs"
 rm -r "$HOME/.config/walker"
-cp -ru "$CONFIG_DIR/walker" "$LOCAL_CONFIG"
-cp -ru "$CONFIG_DIR/elephant" "$LOCAL_CONFIG"
+cp -r "$CONFIG_DIR/walker" "$LOCAL_CONFIG"
+cp -r "$CONFIG_DIR/elephant" "$LOCAL_CONFIG"
+
+prompt "Re-applying theme after updates"
+cp -r "$THEME_DIR/$current_theme" "$LOCAL_CONFIG"
 
 gum confirm "Update complete. Press enter to close."
+
+pkill walker
+systemctl --user restart walker.service elephant.service
