c0eee3e06246cdc5dfd99b4040f4b5def5ce64a3
tyvren <steve.conrad14@gmail.com>
2025-11-30 12:07:39 -0500

    added additional checks to disk management script along with disk selection and auto-fstab creation without user input

--- modules/diskmanagement/mount_disk.sh
+++ modules/diskmanagement/mount_disk.sh
@@ -19,24 +19,47 @@ confirm() {
   gum confirm "$1"
 }
 
-prompt "Indentifying file systems.."
+prompt "Indentifying file systems:"
 disks=$(lsblk -f)
 prompt "$disks"
 
-#Create a mount point for the drive
-prompt "Create a new directory to mount the disk"
-mount_point=$(input "Enter the name for your mount point directory: ex: games")
-sudo mkdir /mnt/"$mount_point"
+prompt "Create a new directory or enter an existing directory to mount the disk."
+mount_point=$(input "Enter the name of your mount point directory: ex: games")
+current_dirs=$(ls /mnt/)
 
-prompt "fstab example: UUID=af8e4d25-b6e0-4bdb-9efd-58d671863f0b /mnt/games ext4 defaults 0 2"
-fstab_input=$(input "Enter the file system UUID, MountPoint, FSType, defaults, 0 2")
+if [[ "$current_dirs" == *"$mount_point"* ]]; then
+  prompt "Mount point already exists. Using existing directory."
+  chosen_mnt="/mnt/$mount_point"
+else
+  prompt "Creating new mount point."
+  sudo mkdir /mnt/"$mount_point"
+  chosen_mnt="/mnt/$mount_point"
+fi
 
-#Append to fstab
-echo "UUID=$fstab_input" | sudo tee -a /etc/fstab
+disks_filtered=$(lsblk -n -o NAME,UUID,MOUNTPOINT,FSTYPE | grep -v '^zram')
+mapfile -t choices <<<"$disks_filtered"
+prompt "Select the disk to mount:"
+disk_choice=$(printf "%s\n" "${choices[@]}" | gum choose --limit=1)
+uuid=$(echo "$disk_choice" | awk '{print $2}')
+fstype=$(echo "$disk_choice" | awk '{print $4}')
+#fstab example: UUID=af8e4d25-b6e0-4bdb-9efd-58d671863f0b /mnt/games ext4 defaults 0 2
+
+#Check if disk already exists in fstab
+fstab_contents=$(</etc/fstab)
+if [[ $fstab_contents == *"$disk_choice"* ]]; then
+  if confirm "Disk already exists in fstab, would you like to overwrite? Select No to cancel."; then
+    prompt "Overwriting fstab entry."
+    sed -i "s|^UUID=$uuid .*|UUID=$uuid $chosen_mnt $fstype defaults 0 2|" /etc/fstab
+  else
+    exit 0
+  fi
+else
+  prompt "Disk not found in fstab file, continuing update."
+  echo "UUID=$uuid $chosen_mnt $fstype defaults 0 2" | sudo tee -a /etc/fstab
+fi
 
 sudo systemctl daemon-reload
-
-show_fstab=$(cat /etc/fstab)
+show_fstab=$(cat -s /etc/fstab)
 prompt "Fstab file updated"
 prompt "$show_fstab"
 
