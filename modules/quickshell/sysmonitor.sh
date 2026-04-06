977a2853128d0b99cab5d478580c7b689b440e36
Tyvren <steve.conrad14@gmail.com>
2026-04-06 15:28:09 -0400

    fixed cpu usage source data

--- modules/quickshell/sysmonitor.sh
+++ modules/quickshell/sysmonitor.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-prev=$(grep 'cpu ' /proc/stat)
-sleep 1
-curr=$(grep 'cpu ' /proc/stat)
-
-prev_idle=$(echo "$prev" | awk '{print $5}')
-prev_total=$(echo "$prev" | awk '{for(i=2;i<=NF;i++)s+=$i;print s}')
-curr_idle=$(echo "$curr" | awk '{print $5}')
-curr_total=$(echo "$curr" | awk '{for(i=2;i<=NF;i++)s+=$i;print s}')
-
-idle_delta=$((curr_idle - prev_idle))
-total_delta=$((curr_total - prev_total))
-
-[ $total_delta -gt 0 ] && cpu_usage=$((100 * (total_delta - idle_delta) / total_delta)) || cpu_usage=0
-
+cpu_usage=$(top -bn1 | grep "%Cpu(s)" | awk '{print $2}')
 ram_usage=$(awk '/MemAvailable/{a=$2} /MemTotal/{t=$2} END{print (t-a)*100/t}' /proc/meminfo)
 
 echo "$cpu_usage $ram_usage"
