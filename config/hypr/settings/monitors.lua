911f2fbb9f8804ee4d091de9efe7f93f344ff27e
Tyvren <steve.conrad14@gmail.com>
2026-05-12 14:04:36 -0400

    fixed xwayland scaling

--- config/hypr/settings/monitors.lua
+++ config/hypr/settings/monitors.lua
@@ -2,10 +2,15 @@
 ---- MONITORS ----
 ------------------
 
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
 	output = "",
 	mode = "preferred",
 	position = "auto",
 	scale = "1.5",
+})
+
+hl.config({
+	xwayland = {
+		force_zero_scaling = true,
+	},
 })
