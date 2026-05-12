a637811af33670dc8db58bf09ff6d360beceab72
Tyvren <steve.conrad14@gmail.com>
2026-05-11 20:40:29 -0400

    changed default monitor settings to use special values for mode

--- config/hypr/settings/monitors.lua
+++ config/hypr/settings/monitors.lua
@@ -5,7 +5,9 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
 	output = "",
-	mode = "3840x2160@240",
+	mode = "preferred",
+	"highres",
+	"highrr",
 	position = "auto",
 	scale = "1.5",
 })
