98fb2dc269d6096ce9c99c3d075265510c4a819d
Tyvren <steve.conrad14@gmail.com>
2026-05-16 17:57:34 -0400

    updated monitor script to source lua data for monitor

--- modules/quickshell/qs_apply_monitors.sh
+++ modules/quickshell/qs_apply_monitors.sh
@@ -6,12 +6,24 @@ POS=$3
 SCALE=$4
 GDK=$5
 
-CONF_FILE="$HOME/.config/hypr/settings/monitors.conf"
+CONF_FILE="$HOME/.config/hypr/settings/monitors.lua"
 
-if grep -q "monitor=$NAME," "$CONF_FILE"; then
-  sed -i "s|^monitor=$NAME,.*|monitor=$NAME,$RES,$POS,$SCALE|" "$CONF_FILE"
+if grep -q "output = \"$NAME\"" "$CONF_FILE"; then
+  sed -i "/output = \"$NAME\"/,/})/ s/mode = .*/mode = \"$RES\",/" "$CONF_FILE"
+  sed -i "/output = \"$NAME\"/,/})/ s/position = .*/position = \"$POS\",/" "$CONF_FILE"
+  sed -i "/output = \"$NAME\"/,/})/ s/scale = .*/scale = \"$SCALE\",/" "$CONF_FILE"
 else
-  sed -i "/monitor=,preferred,auto, 1/a monitor=$NAME,$RES,$POS,$SCALE" "$CONF_FILE"
+  cat <<EOF >>"$CONF_FILE"
+
+hl.monitor({
+    output = "$NAME",
+    mode = "$RES",
+    position = "$POS",
+    scale = "$SCALE",
+})
+EOF
 fi
 
-sed -i "s|^env = GDK_SCALE,.*|env = GDK_SCALE,$GDK|" "$CONF_FILE"
+sed -i "s/env = GDK_SCALE, .*/env = GDK_SCALE, $GDK,/" "$CONF_FILE"
+
+hyprctl reload
