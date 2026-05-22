0441d5e413dbae74ab82234cda4006e388700c33
Tyvren <steve.conrad14@gmail.com>
2026-05-22 09:24:49 -0400

    removed hyprlock

--- modules/style/wallpaper_changer.sh
+++ modules/style/wallpaper_changer.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 WALLPAPER_PATH="$1"
-HYPRLOCK_CONF="$HOME/.config/hypr/hyprlock.conf"
 
 if [ -n "$WALLPAPER_PATH" ]; then
   echo "$WALLPAPER_PATH" >"$STATE_FILE"
-
-  if [ -f "$HYPRLOCK_CONF" ]; then
-    sed -i "/background {/,/}/{s|path = .*|path = $WALLPAPER_PATH|}" "$HYPRLOCK_CONF"
-  fi
 
   notify-send "Wallpaper Updated" "$WALLPAPER_PATH"
 else
