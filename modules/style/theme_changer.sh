9fe220fc98f02ab9c2a602903c2e98ce12aea7e0
Tyvren <steve.conrad14@gmail.com>
2026-05-22 09:02:06 -0400

    removed hyprlock

--- modules/style/theme_changer.sh
+++ modules/style/theme_changer.sh
@@ -2,7 +2,6 @@
 
 THEME_DIR="$HOME/.config/hyprnosis/themes"
 WALLPAPER_DIR="$HOME/.config/hyprnosis/wallpapers"
-HYPRLOCK_CONF="$HOME/.config/hypr/hyprlock.conf"
 
 if [[ -z "$1" ]]; then
   exit 1
@@ -20,12 +19,6 @@ if [[ ! -d "$THEME_PATH" ]]; then
   exit 1
 fi
 
-shopt -s nullglob
-WALLPAPERS=("$WALL_PATH"/*.png "$WALL_PATH"/*.jpg)
-if ((${#WALLPAPERS[@]} > 0)); then
-  sed -i "/background {/,/}/{s|^\s*path = .*|  path = ${WALLPAPERS[0]}|}" "$HYPRLOCK_CONF"
-fi
-
 cp -r "$THEME_PATH/"* "$HOME/.config/"
 
-notify-send "Theme Changed" "System theme '$SELECTED_THEME' applied."
+notify-send "Theme Changed" "$SELECTED_THEME"
